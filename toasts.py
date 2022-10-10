21bf562c88ecd55d3fa959ab9eda56fae44481ae
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2022-10-10 22:47:40 +0200

    Add toasts declaration multiple language support

--- toasts.py
+++ toasts.py
@@ -14,6 +14,53 @@ class Toasts(commands.Cog):
         bot
     ):
         self.bot = bot
+        self.languages = [
+            ToastLanguage(
+                "cs-CZ",
+                ":flag_cz:",
+                "Toasty číslo",
+                "Toasty číslo 69    Naaajssss",
+                "Toasty pro ",
+                "Toasty došly.",
+            ),
+            ToastLanguage(
+                "sk-SK",
+                ":flag_sk:",
+                "Tousty číslo",
+                "Tousty číslo 69    Naaajssss",
+                "Tousty pre ",
+                "Tousty sú v piči.",
+            ),
+            ToastLanguage(
+                "en-US",
+                ":flag_us:",
+                "Toasts number",
+                "Toasts number 69    Nice",
+                "Toasts for",
+                "Toasts are out of stock.",
+            ),
+        ]
+
+    def get_language_names(self):
+        names = []
+        for lang in self.languages:
+            names.add(lang.name)
+        return names
+
+    async def autocomp_language_names(
+        self,
+        inter: disnake.ApplicationCommandInteraction,
+        user_input: str
+    ):
+        return [
+            lang for lang in self.languages if user_input.lower() in lang.name
+        ]
+
+    def get_language_by_name(self, name: str):
+        for lang in self.languages:
+            if lang.name == name:
+                return lang
+        return None
 
     @commands.slash_command(description="Vyhlásí tousty.")
     async def toasts(
@@ -23,42 +70,52 @@ class Toasts(commands.Cog):
             ge=0,
             le=200,
             name="number",
-            description="číslo volaných toustů"
+            description="Číslo volaných toustů"
         ),
-        # recipient: str = commands.Param(
-        #     name="recipient",
-        #     description="jméno příjemce volaných toustů",
-        #     default=None
-        # ),
+        recipient: str = commands.Param(
+            name="recipient",
+            description="Jméno příjemce volaných toustů v 2. pádě (Pro koho? Čeho?)",
+            default=None
+        ),
         lang: str = commands.Param(
             name="language",
-            description="jazyk pro vyhlášení toustů",
+            description="Jazyk pro vyhlášení toustů",
             default="cs-CZ",
-            choices=["cs-CZ", "en-US", "sk-SK"]
+            choices=get_language_names(),
+            autocomplete=autocomp_language_names
         )
     ):
         await inter.response.defer()
 
-        logging.info(f"Going to declare toasts {number} in {lang}.")
-        full_filename = f"toasts-{lang}-{number}.wav"
-        if lang == "cs-CZ":
-            text = f"Toasty číslo {number}"
-            flag_emote = ":flag_cz:"
-            if number == 69:
-                text = "Toasty číslo 69    Naaajssss"
-        elif lang == "en-US":
-            text = f"Toast number {number}"
-            flag_emote = ":flag_us:"
-            if number == 69:
-                text = "Toasts number 69    Nice"
-        elif lang == "sk-SK":
-            text = f"Tousty číslo {number}"
-            flag_emote = ":flag_sk:"
-            if number == 69:
-                text = "Toasty číslo 69    Naajsss"
+        # Get language object
+        language = self.get_language_by_name(lang)
 
+        # Prepare variables for toasts declaring
+        if recipient is not None:
+            # Recipient way
+            logging.info(f"Going to declare toasts for \"{recipient}\" in \"{lang}\".")
+            full_filename = f"toasts-for-recipient-{number}.{lang}.wav"
+            number = -1
+            text = f"{language.recipient_toasts_text} {recipient}"
+            response_text = f":arrow_right: \"{recipient}\""
+        else:
+            # Number way
+            logging.info(f"Going to declare toasts {number} in \"{lang}\".")
+            full_filename = f"toasts-number-{number}.{lang}.wav"
+            text = f"{language.default_toasts_text} {number}"
+            response_text = str(number)
+            if number == 69:
+                text = language.special_toasts_text
+                response_text = f"{number} <:nepSmug:827833315822141451>"
+            elif number == 0:
+                text = language.gone_toasts_text
+                response_text = f"{language.gone_toasts_text} <:sadcat:576171980118687754>"
+
+        # Add flag emote by language to response
+        response_text += f" {language.flag_emote}"
+
+        # If file is not in local directory create it
         if not exists(full_filename):
-            # If file is not in local directory create it
             q = requests.utils.quote(text)
             request = requests.get(
                 f"https://translate.google.com/translate_tts?ie=UTF-8&tl={lang}&client=tw-ob&q={q}"
@@ -69,6 +126,7 @@ class Toasts(commands.Cog):
             sound = AudioSegment.from_mp3(f"{Config.toasts_sounds_path}/{full_filename[:-4]}.mp3")
             sound.export(f"{Config.toasts_sounds_path}/{full_filename}", format="wav")
 
+        # Define sound object
         sound_to_play = Sound(
             name=text,
             files=[
@@ -77,10 +135,23 @@ class Toasts(commands.Cog):
             spotify=Config.spotify,
         )
 
-        # Play the sound
+        # Play the sound object
         sound_to_play.play_fade_out()
 
-        # Update message
+        # Send response message to channel
         await inter.followup.send(
-            content=f":white_check_mark: :sandwich: {number} {flag_emote}"
+            content=f":white_check_mark: :sandwich: {response_text}"
         )
+
+
+class ToastLanguage:
+    def __init__(
+        self,
+        name: str,
+        flag_emote: str,
+        default_toasts_text: str,
+        special_toasts_text: str, # 69
+        recipient_toasts_text: str,
+        gone_toasts_text: str,
+    ):
+        pass
